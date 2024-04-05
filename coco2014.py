f14c3110edbfc6a5abfe8bc92441aa0a7a100405
KimRass <rmx1000@yonsei.ac.kr>
2024-04-05 17:58:11 +0900

    Working DualTransform...

--- coco2014.py
+++ coco2014.py
@@ -3,6 +3,11 @@ import numpy as np
 from torchvision.datasets import CocoDetection
 import random
 from PIL import Image
+import torchvision.transforms as T
+from torchvision.transforms import v2
+import albumentations as A
+from albumentations.core.transforms_interface import DualTransform
+import random
 
 
 def to_pil(img):
@@ -26,9 +31,109 @@ def get_color(obj_idx, cls_idx, palette, task):
     if task == "instance":
         return palette[obj_idx]
     elif task == "semantic":
-        return palette[cls_idx % len(palette)]
+        # return palette[cls_idx % len(palette)]
+        return palette[cls_idx]
 
 
+def get_mask(h, w, polys):
+    mask = np.zeros((h, w), dtype=np.uint8)
+    for poly in polys:
+        poly = np.array(poly).reshape((-1, 2)).astype(np.int32)
+        cv2.fillPoly(mask, pts=[poly], color=255)
+    return mask
+
+
+def colorize_mask(mask, color):
+    colored_mask = np.stack([mask] * 3, axis=-1)
+    for i in range(3):
+        colored_mask[..., i][colored_mask[..., i] == 255] = color[i]
+    return colored_mask
+
+
+def overlay_mask(img, colored_mask, beta):
+    alpha = ((colored_mask > 0).max(axis=2) * 128).astype(np.uint8)
+    rgba_mask = np.concatenate([colored_mask, alpha[:, :, None]], axis=2)
+    rgba_img = cv2.cvtColor(img, cv2.COLOR_RGB2RGBA)
+    rgba_img = cv2.addWeighted(rgba_img, 1, rgba_mask, beta, gamma=0)
+    return cv2.cvtColor(rgba_img, cv2.COLOR_RGBA2RGB)
+
+
+def put_cls_name(img, cls_name, poly, color):
+    # cls_id = ds.coco.getCatIds(catIds=[cls_idx])
+    # cls_name = ds.coco.loadCats(cls_id)[0]["name"]
+    x, y = np.mean(poly, axis=0).astype(np.int32)
+    cv2.putText(
+        img,
+        text=cls_name,
+        org=(x, y),
+        fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+        fontScale=0.5,
+        color=color.tolist(),
+        thickness=1,
+    )
+
+
+def vis_label(image, label, palette, beta=0.8):
+    img = to_array(image)
+    for obj_idx, obj in enumerate(label):
+        polys = obj["segmentation"]
+        cls_idx = obj["category_id"]
+
+        if isinstance(polys, list):
+            h, w = img.shape[:2]
+            mask = get_mask(h=h, w=w, polys=polys)
+            color = get_color(
+                obj_idx=obj_idx, cls_idx=cls_idx, palette=palette, task="instance",
+            )
+            colored_mask = colorize_mask(mask, color=color)
+            img = overlay_mask(img, colored_mask=colored_mask, beta=beta)
+    to_pil(img).show()
+
+
+class LargeScaleJittering(DualTransform):
+    def __init__(self, img_size, pad_color=(127, 127, 127), always_apply=False, p=1.0):
+        super().__init__(always_apply=always_apply, p=p)
+
+        self.transform = A.Compose(
+            [
+                A.ShiftScaleRotate(
+                    shift_limit=(-0.5, 0.5),
+                    scale_limit=(-0.9, 1),
+                    rotate_limit=0,
+                    border_mode=cv2.BORDER_CONSTANT,
+                    value=pad_color,
+                    p=1,
+                ),
+                A.PadIfNeeded(
+                    min_height=img_size,
+                    min_width=img_size,
+                    border_mode=cv2.BORDER_CONSTANT,
+                    value=pad_color,
+                ),
+                A.CenterCrop(height=img_size, width=img_size),
+                A.HorizontalFlip(p=0.5),
+            ]
+        )
+
+    def apply(self, img, **params):
+        augmented = self.transform(image=img, **params)
+        return augmented['image']
+
+    def apply_to_mask(self, mask, **params):
+        augmented = self.transform(image=mask, **params)
+        return augmented['image']
+
+    def get_params_dependent_on_targets(self, params):
+        return {}
+
+    @property
+    def targets_as_params(self):
+        return ['image', 'mask']
+
+
+# "We randomly select two images and apply random scale jittering and random horizontal flipping on each of them. Then we select a random subset of objects from one of the images and paste them onto the other image."
+# "we remove fully occluded objects and update the masks and bounding boxes of partially occluded objects."
+# "For composing new objects into an image, we compute the binary mask (α) of pasted objects using ground-truth annotations and compute the new image as I1 × α + I2 × (1 − α) where I1 is the pasted image and I2 is the main image. To smooth out the edges of the pasted objects we apply a Gaussian filter to α similar to “blending” in [13]. But unlike [13], we also found that simply compos- ing without any blending has similar performance."
 if __name__ == "__main__":
     data_dir = "/home/jbkim/Documents/datasets/val2014"
     instances_file = "/home/jbkim/Documents/datasets/annotations_trainval2014/annotations/instances_val2014.json"
@@ -39,43 +144,89 @@ if __name__ == "__main__":
     N_CLASSES = 80
     palette = get_palette(N_CLASSES)
 
+
+
     image, label = next(di)
-    # image.show()
+    # new_image = vis_label(image, label=label, palette=palette)
 
-    img = to_array(image)
-    for obj_idx, obj in enumerate(label):
-        segmentation = obj["segmentation"]
-        bbox = obj["bbox"]
+    img_size = 512
+    transform = LargeScaleJittering(img_size=img_size)
+
+    image1, label1 = next(di)
+
+    # image1.show()
+    img1 = to_array(image1)
+    img1 = transform(image=img1)["image"]
+    img1.shape
+
+    image2, label2 = next(di)
+    
+    img2 = to_array(image2)
+    prob = 0.3
+    h, w = img2.shape[:2]
+    new_mask = np.zeros((h, w, 3), dtype=np.uint8)
+    for obj_idx, obj in enumerate(label2):
+        if random.random() > prob:
+            continue
+        polys = obj["segmentation"]
         cls_idx = obj["category_id"]
+        mask = get_mask(h=h, w=w, polys=polys)
+        mask = np.stack([mask] * 3, axis=-1)
 
-        bbox = [int(coord) for coord in bbox]
+        new_mask = np.maximum(new_mask, mask)
 
-        color = get_color(obj_idx=obj_idx, cls_idx=cls_idx, palette=palette, task="instance")
-        if isinstance(segmentation, list):
-            h, w = img.shape[:2]
+    transformed2 = transform(image=img2, mask=new_mask)
+    img2 = transformed2["image"]
+    new_mask = transformed2["mask"]
+    # to_pil(img2).show()
 
-            mask = np.zeros((h, w), dtype=np.uint8)
-            for poly in segmentation:
-                poly = np.array(poly).reshape((-1, 2)).astype(np.int32)
-                cv2.fillPoly(mask, pts=[poly], color=255) # color is not specified here
-            # to_pil(mask).show()
+    new_img = np.where(new_mask == 255, img2, img1)
+    to_pil(new_img).show()
+    to_pil(new_mask).show()
+    # to_pil(img1).show()
 
-            mask_color = np.stack([mask] * 3, axis=-1)
-            mask.shape, mask_color.shape
-            for i in range(3):
-                mask_color[..., i][mask_color[..., i] == 255] = color[i]
 
-            alpha = ((mask_color > 0).max(axis=2) * 128).astype(np.uint8)
-            rgba_mask = np.concatenate([mask_color, alpha[:, :, np.newaxis]], axis=2)
 
-            img_rgba = cv2.cvtColor(img, cv2.COLOR_RGB2RGBA)
-            img_rgba = cv2.addWeighted(img_rgba, 1, rgba_mask, 0.8, 0)
 
-            img = cv2.cvtColor(img_rgba, cv2.COLOR_RGBA2RGB)
 
-        cls_id = ds.coco.getCatIds(catIds=[cls_idx])
-        cls_name = ds.coco.loadCats(cls_id)[0]["name"]
-        # cv2.putText(
-        #     img, cls_name, (bbox[0], bbox[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2,
-        # )
-    to_pil(img).show()
+    class CustomTransform(A.DualTransform):
+        def __init__(self, img_size, pad_color=(0, 0, 0), always_apply=False, p=1.0):
+            super().__init__(always_apply=always_apply, p=p)
+            
+            self.transforms = A.Compose([
+                A.ShiftScaleRotate(
+                    shift_limit=(-0.5, 0.5),
+                    scale_limit=(-0.9, 1),
+                    rotate_limit=0,
+                    border_mode=cv2.BORDER_CONSTANT,
+                    value=pad_color,
+                    p=1,
+                ),
+                A.PadIfNeeded(
+                    min_height=img_size,
+                    min_width=img_size,
+                    border_mode=cv2.BORDER_CONSTANT,
+                    value=pad_color,
+                ),
+                A.CenterCrop(height=img_size, width=img_size),
+                A.HorizontalFlip(p=0.5),
+            ])
+
+        def apply(self, img, **params):
+            return self.transforms(image=img, **params)
+
+        def apply_to_mask(self, mask, **params):
+            return self.transforms(image=mask, **params)
+
+        def get_transform_init_args_names(self):
+            return ('img_size', 'pad_color')
+        @property
+        def targets_as_params(self):
+            return []
+        
+    tt = CustomTransform()
+    img1 = to_array(image1)
+    new_mask = np.zeros((h, w, 3), dtype=np.uint8)
+    # out = tt(image=img1, mask=new_mask)
+    out = tt(image=img1)
+    # out.keys()
