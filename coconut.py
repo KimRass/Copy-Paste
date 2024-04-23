4826cde33af27b03b02abbf32c529f1f76add791
KimRass <rmx1000@yonsei.ac.kr>
2024-04-23 09:09:42 +0900

    Pre-processing CocoNut

--- coconut.py
+++ coconut.py
@@ -1,55 +1,47 @@
 # Sources:
     # https://www.kaggle.com/datasets/xueqingdeng/coconut/
-    # https://github.com/bytedance/coconut_cvpr2024?tab=readme-ov-file
+    # https://github.com/bytedance/coconut_cvpr2024
 
 from coco import COCODS
+from pycocotools.coco import COCO
+import json
+from pathlib import Path
+from PIL import Image
+import numpy as np
+import cv2
+import einops
+import torch
+from collections import defaultdict
+import pandas as pd
+annot_path = "/home/jbkim/Documents/datasets/coconut/coconut_dataset/annotations/annotations/coconut_s.json"
+img_dir = "/home/jbkim/Documents/datasets/coconut/train2017 (1)/train2017"
+mask_dir = "/home/jbkim/Documents/datasets/coconut/coconut_dataset/coconut_s/coconut_s/"
+
+with open(annot_path, mode="r") as f:
+    coconut = json.load(f)
+coconut.keys()
+img_dicts = coconut["images"]
 
 
-if __name__ == "__main__":
-    from pycocotools.coco import COCO
-    import json
-    from pathlib import Path
-    from PIL import Image
-    import numpy as np
-    import cv2
-    import einops
-    annot_path = "/home/jbkim/Documents/datasets/coconut/coconut_dataset/annotations/annotations/coconut_s.json"
-    img_dir = "/home/jbkim/Documents/datasets/coconut/train2017 (1)/train2017"
-    mask_dir = "/home/jbkim/Documents/datasets/coconut/coconut_dataset/coconut_s/coconut_s/"
-    
-    with open(annot_path, mode="r") as f:
-        coconut = json.load(f)
-    coconut.keys()
-    img_dicts = coconut["images"]
-    
-    
-    
-    
-    idx = 3
-    img_dict = img_dicts[idx]
-    img_id = img_dict["id"]
-    img_path = Path(img_dir)/img_dict["file_name"]
 
-    annots = coconut["annotations"]
-    for annot in annots:
-        mask_path = Path(mask_dir)/annot["file_name"]
-        img_id = annot["image_id"]
-        # break
-    annots[0]
-    
-    
-    mask_path = "/home/jbkim/Documents/datasets/coconut/coconut_dataset/coconut_s/coconut_s/000000000009.png"
-    # mask = cv2.imread(mask_path, cv2.IMREAD_GRAYSCALE)
-    mask = cv2.imread(mask_path, cv2.IMREAD_COLOR)
-    mask.shape
-    np.unique(mask)
-    np.unique(mask[..., 0])
-    mask = mask[..., 0][None, ...]
-    # mask.shape
-    _, h, w = mask.shape
-    
-    repeated_mask = einops.repeat(
-        np.unique(mask), pattern="i -> i h w", h=h, w=w,
-    )
-    repeated_mask.shape
-    (repeated_mask == mask).shape
+annot_dict = defaultdict(str)
+annots = coconut["annotations"]
+for annot in annots:
+    mask_path = Path(mask_dir)/annot["file_name"]
+    img_id = annot["image_id"]
+    annot_dict[img_id] = str(mask_path)
+    pd.DataFrame(annot["segments_info"])
+
+
+idx = 3
+img_dict = img_dicts[idx]
+img_id = img_dict["id"]
+img_path = Path(img_dir)/img_dict["file_name"]
+Image.open(img_path).show()
+
+
+mask_path = annot_dict[img_id]
+mask = cv2.imread(mask_path, cv2.IMREAD_COLOR)[..., 2]
+bin_mask = mask[None, ...] == np.unique(mask)[..., None, None]
+# Image.fromarray(mask[..., 2] * 20).show()
+
